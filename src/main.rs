e629543fbc2e88364800f92c82e45542f6c51955
boris <boris.farber@gmail.com>
2026-08-29 11:15:06 +0300

    added file walker when there is no pipe

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,47 @@
+use std::env;
 use std::io::stdin;
+use std::io::{self, IsTerminal};
 
 use fuzz::fuzzyfinder::ui::Picker;
+use walkdir::DirEntry;
+use walkdir::WalkDir;
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
 
 fn main() {
     let mut picks: Vec<String> = vec![];
 
-    for line in stdin().lines() {
-        let line = line.expect("Error reading stdin").trim().to_string();
-        picks.push(line);
+    if io::stdin().is_terminal() {
+        // Read the current directory ("./")
+        //picks = fs::read_dir("/home/boris/Development/Fuzz/target/debug")
+        //    .unwrap()
+        //    .filter_map(|e| e.ok())
+        //    .map(|e| e.path().to_string_lossy().into_owned())
+        //    .collect::<Vec<_>>();
+
+        let m = env::current_dir()
+            .expect("Failed to get current directory")
+            .to_string_lossy()
+            .into_owned();
+
+        let walker = WalkDir::new(&m).into_iter();
+        for entry in walker.filter_entry(|e| !is_hidden(e)) {
+            //println!("{}", entry?.path().display());
+            if let Some(path) = entry.ok().map(|e| e.path().to_string_lossy().into_owned()) {
+                picks.push(path);
+            }
+        }
+    } else {
+        for line in stdin().lines() {
+            let line = line.expect("Error reading stdin").trim().to_string();
+            picks.push(line);
+        }
     }
 
     let mut picker = Picker::new(picks);
